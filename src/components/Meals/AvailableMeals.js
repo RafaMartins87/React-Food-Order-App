5db323b52356e96133e286864679c24932a31b85
rafael martins <rafael.martins87@outlook.com>
2022-07-22 00:23:54 -0300

    chore(22-07): improving meals list item

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import Card from "../UI/Card";
 import classes from './AvailableMeals.module.css';
+import MealItem from "./MealItem/MealItem";
 
 const DUMMY_MEALS = [
     {
@@ -28,16 +29,23 @@ const DUMMY_MEALS = [
     },
 ];
 
-const mealsList = DUMMY_MEALS.map(item => <li>{item.name}</li>);
-
 const AvailableMeals = () => {
+    const mealsList = DUMMY_MEALS.map((meal) => (
+        <MealItem
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        />
+    ));
+
     return (
         <section className={classes.meals}>
             <Card>
-        <ul>
-        {mealsList}
-    </ul>
-    </Card>
+                <ul>
+                    {mealsList}
+                </ul>
+            </Card>
         </section>
     )
 }
